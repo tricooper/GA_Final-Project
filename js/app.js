8dfdc81398fcf812f03a227b85d857b9b1b2861f
Tristan <tricooper@gmail.com>
2017-07-23 13:16:35 -0400

    hello

--- js/app.js
+++ js/app.js
@@ -31,3 +31,55 @@
       ]
       });
     });
+
+
+// search based on input for lat/long
+
+    $('#fly-button').on('click', function() {
+    var destination = $('#destination').val();
+    $('#destination').val('');
+    console.log(destination);
+     var request = $.ajax({
+      url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + destination + '&key=AIzaSyDu3gjh4psHcCnMHJYhew2EebBb3I_jdAQ',
+      success: appendDestination
+       
+     }); 
+   });
+
+    function appendDestination (data) {
+    var latitude = data.results[0].geometry.bounds.northeast.lat;
+    var longitude = data.results[0].geometry.bounds.northeast.lng;
+    console.log(latitude);
+    console.log(longitude);
+    $('.geolocation').html('');
+    $('.geolocation').append('<div>'+ latitude +'</div>' + longitude);
+
+
+
+  } 
+
+
+// on Keyup search for a location
+
+$('#origin').on('keyup', function(){
+  var query = $('#origin').val();
+  originAutocomplete(query);
+});
+
+var originAutocomplete = function (query) {
+  var request = $.ajax({
+    url:  'https://iatacodes.org/api/v6/autocomplete?query=' + query + '&api_key=beb637d9-aeed-4a1f-9dbc-4a98218fa7c8',
+    contentType: "application/json",
+    success: appendAutocomplete
+  });
+
+}
+
+var appendAutocomplete = function(data) {
+  console.log(data);
+}
+
+// search for flights using Google flights
+
+
+
